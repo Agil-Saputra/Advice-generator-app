a1a034a1ec80b656f3b75c279a5438bff4535631
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2022-12-25 23:39:26 +0700

    add loader

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const quote = document.getElementById('advice');
 const btn = document.getElementById('btn');
 const order = document.getElementById('order');
 
-btn.addEventListener('click', get)
-
 function get() {
     fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
@@ -12,3 +10,19 @@ function get() {
         quote.innerText ='"' + data.slip.advice + '"';
     })
 }
+
+const spinner = document.querySelector('.spinner');
+
+
+
+btn.addEventListener('click', () => {
+    spinner.style.display = 'grid';
+    quote.style.display = 'none';
+    order.style.display = 'none';
+    get();
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        quote.style.display = 'grid';
+        order.style.display = 'block';
+    }, 1500)
+})
